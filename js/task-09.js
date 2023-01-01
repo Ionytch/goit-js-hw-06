42eccd4c93aac2b3df3953b9d4fe997ee9c55867
Aleks Ionin <starik.ionytch@gmail.com>
2023-01-01 21:56:43 +0200

    task 06 updated

--- js/task-09.js
+++ js/task-09.js
@@ -1,14 +1,15 @@
 const buttonColor = document.querySelector('button.change-color')
 const bodyColor = document.querySelector('body');
 const spanColor = document.querySelector('span.color');
+
 buttonColor.addEventListener('click', () => {
+  
+  function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
   spanColor.textContent = getRandomHexColor();
-  return bodyColor.style.backgroundColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = getRandomHexColor();
 });
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-
-
-
-}
